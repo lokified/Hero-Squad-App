a19bf85799d1d7a59125d8b5d3c6db77bba69f2f
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-30 16:51:03 +0300

    add constructor,getters and setters

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -5,4 +5,42 @@ public class Hero {
     private String specialPower;
     private String weakness;
 
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public void setSpecialPower(String specialPower) {
+        this.specialPower = specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
 }
