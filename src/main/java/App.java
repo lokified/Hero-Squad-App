1645e788318a57cd7e7d9e79208a520d163684a9
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-30 22:36:38 +0300

    add routes for form and homepage

---
+++ src/main/java/App.java
@@ -0,0 +1,45 @@
+import models.Hero;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        //shows homepage
+        get("/",(request,response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //show form to add hero
+        get("/form",(request, response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model,"hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //process new form
+        post("/form/new",(request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+            String powers = request.queryParams("powers");
+            String weakness = request.queryParams("weakness");
+
+            Hero newHero = new Hero(name,age,powers,weakness);
+            model.put("heros",newHero);
+
+            return new ModelAndView(model,"success.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
