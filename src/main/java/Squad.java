3aa8ba206c092cff4ddbb99a238c9041ac953458
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-30 17:18:24 +0300

    add properties,constructors  and getters and setters

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class Squad {
+
+    private String name;
+    private int numberOfHeros;
+    private String mission;
+
+    public Squad(String name, int numberOfHeros, String mission) {
+        this.name = name;
+        this.numberOfHeros = numberOfHeros;
+        this.mission = mission;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfHeros() {
+        return numberOfHeros;
+    }
+
+    public void setNumberOfHeros(int numberOfHeros) {
+        this.numberOfHeros = numberOfHeros;
+    }
+
+    public String getMission() {
+        return mission;
+    }
+
+    public void setMission(String mission) {
+        this.mission = mission;
+    }
 }
